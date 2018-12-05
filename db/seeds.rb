7f8ad47ac4f5e7ac69131b6094ffbba81673b796
github username <github email address>
2018-12-04 19:00:19 -0500

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(first_name: "kevy", last_name: "wevy")
+feedback1 = Feedback.create(user_id: 3, comment: "why are you not here", colleague_id: 1)
